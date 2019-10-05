5c5542c2502c36f00d145ba5c915489b1741c54d
Nathan Alex <nate745@gmail.com>
2019-10-05 21:51:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,24 @@
-def map(s)
+def map(array)
   new = []
   i = 0
-  while i < s.length
-    new.push(yield(s[i]))
+  while i < array.length
+    new.push(yield(array[i]))
     i += 1
   end
   new
+end
+
+def reduce(x, starting_point= 0 )
+  if starting_point
+    aggregate = starting_point
+    i = 0
+  else
+    aggregate = x[0]
+    i = 1
+  end
+  while i < s.length
+    aggregate = yield(aggregate, s[i])
+    i += 1
+  end
+  aggregate
 end
