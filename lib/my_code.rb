e798de68c685d1d40cf35071238a243699bf8f1b
Nathan Alex <nate745@gmail.com>
2019-10-05 21:54:49 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+def map(array)
+  new = []
+  i = 0
+  while i < array.length
+    new.push(yield(array[i]))
+    i += 1
+  end
+  new
+end
+
+def reduce(x, starting_point= nil )
+  if starting_point
+    aggregate = starting_point
+    i = 0
+  else
+    aggregate = x[0]
+    i = 1
+  end
+  while i < x.length
+    aggregate = yield(aggregate, x[i])
+    i += 1
+  end
+  aggregate
+end
